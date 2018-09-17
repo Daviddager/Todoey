c98df1916352225065f1b784673e8bbab1c6d0af
David Dager <ddager@rccl.com>
2018-09-17 16:45:18 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by David Ricardo Dager Mora on 9/17/18.
+//  Copyright © 2018 David Ricardo Dager Mora. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
